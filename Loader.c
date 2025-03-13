ff35148f5ab120164790117e2ea919e8cc8076d9
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-03-13 23:24:39 +0100

    sdfaisnfdsjfhasibcvuweybhfviuwenf whefuwhke

--- Loader.c
+++ Loader.c
@@ -1,5 +1,7 @@
 #include "Loader.h"
 
+#include <string.h>
+
 void LoadMlxBindings(mlx_context mlx, MlxFuncs* mlx_funcs)
 {
 	#define MLX_FUNCTION(fn) mlx_funcs->fn = (PFN_##fn)mlx_get_proc_addr(mlx, #fn);
@@ -18,9 +20,16 @@ void LoadVulkanBindings(mlx_context mlx, MlxFuncs* mlx_funcs, VulkanFuncs* vulka
 	#undef VULKAN_DEVICE_FUNCTION
 }
 
-PFN_vkVoidFunction ImGuiLoadVulkan(const char* fn, void* data)
+PFN_vkVoidFunction ImGuiLoadVulkan(const char* name, void* data)
 {
-	return GetVulkanFuncs()->vkGetInstanceProcAddr((VkInstance)data, fn);
+	#define VULKAN_GLOBAL_FUNCTION(fn)   if(strcmp(name, #fn) == 0) return (PFN_vkVoidFunction)GetVulkanFuncs()->fn;
+	#define VULKAN_INSTANCE_FUNCTION(fn) if(strcmp(name, #fn) == 0) return (PFN_vkVoidFunction)GetVulkanFuncs()->fn;
+	#define VULKAN_DEVICE_FUNCTION(fn)   if(strcmp(name, #fn) == 0) return (PFN_vkVoidFunction)GetVulkanFuncs()->fn;
+		#include "VulkanDefs.h"
+	#undef VULKAN_GLOBAL_FUNCTION
+	#undef VULKAN_INSTANCE_FUNCTION
+	#undef VULKAN_DEVICE_FUNCTION
+	return GetVulkanFuncs()->vkGetInstanceProcAddr((VkInstance)data, name);
 }
 
 MlxFuncs* GetMlxFuncs()
